17c6993fe2a18ae9f39606ef980f20ae44a3ff98
Gaoxiang (Shawn) Li <ligaoxiang1995@gmail.com>
2026-03-29 14:28:19 -0500

    update
    Removed the implementation of count_isolated function.

--- lab06/Question2/student.c
+++ lab06/Question2/student.c
@@ -13,24 +13,7 @@
 int count_isolated(Graph* g) {
     // TODO: implement
     // return -1;
-    int count = 0;
-
-    for (int i = 0; i < MAX_NODES; i++) {
-        int is_isolated = 1;
-
-        for (int j = 0; j < MAX_NODES; j++) {
-            if (g->adj[i][j] == 1) {
-                is_isolated = 0;
-                break;
-            }
-        }
-
-        if (is_isolated) {
-            count++;
-        }
-    }
-
-    return count;
+   
 }
 
 
